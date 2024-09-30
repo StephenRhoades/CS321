d04b85e286f33fded7b1c6fc79093d568dc88fcb
calpyo2454 <160197368+calpyo2454@users.noreply.github.com>
2024-09-30 14:54:01 -0400

    Update calendar.js

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -20,31 +20,45 @@ class Calendar{
   *the 12 months in a single year. 
   */
 class yearCalendar extends Calendar{
-  monthCalendar;
-  constructor()
+  monthCalendar; // monthCalendar Object
+  year; //integer
+  constructor(year)
   {
     this.monthCalendar = new Array(12);
+    this.year = year;
   }
 
   get monthCalendar()
   {
     return this.monthCalendar;
   }
+ 
+  get year()
+  {
+    return this.year;
+  }
+  
 }
 /**
 *Class declaration of month Calendar Object. A  month Calendar is initialized with an array representing the days within a month. 
 *days: The number of days (integer) within a particular month
 */
 class monthCalendar extends yearCalendar{
-  days;
-  constructor(days)
+  days; //integer
+  month; //string
+  constructor(days, month)
   {
     this.days = new Array(days);
+    this.month = month;
   }
 
   get days()
   {
     return this.days;
+  }
+  get month()
+  {
+    return this.month;
   }
 }
 //To-do: Declare Class Days. Days represent a single day within a month, within a year. Days should be contain a sorted linked list of Task objects, sorted by task due date from soonest to latest.
