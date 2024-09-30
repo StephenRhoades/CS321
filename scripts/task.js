e6bd8b0b55ba0f23f240f5de2fcc76cadf02c071
Stephen Rhoades <stephensnw1@gmail.com>
2024-09-30 15:52:30 -0400

    Update task.js

--- scripts/task.js
+++ scripts/task.js
@@ -1,1 +1,21 @@
+Class task {
+  
+  function changeName (name) {
+  }
 
+  function changeDueDate (date) {
+  }
+
+  function changeCategory (category) {
+  }
+
+  function complete () {
+  }
+
+  function changeRecurring (isRecurring) {
+  }
+
+  function changeDescription (description) {
+  }
+
+}
