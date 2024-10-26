89488acb2690de315563f18798b54d3d4db7b6f1
WashtonBin <jiangbin88424@gmail.com>
2024-10-26 13:08:10 -0400

    make functions

---
+++ scripts/taskPage.js
@@ -0,0 +1,9 @@
+// Wait for the DOM to load
+document.addEventListener('DOMContentLoaded', function() {
+    
+});
+
+// Function to display tasks on taskPage.html
+function displayTasks(tasks) {
+    
+}
